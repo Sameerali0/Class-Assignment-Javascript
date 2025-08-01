25dd68533978a4f8fdf6457cf1d76e534bce45d4
Sameerali0 <sameer.ali.professional@gamail.com>
2025-08-01 14:53:06 +0500

    Javascript Intro

---
+++ Javascript-intro-assignment/script.js
@@ -0,0 +1,22 @@
+//var variable with console.log
+var product = "Projector";
+console.log(product);
+
+
+var num1 = 20;
+var num2 = 25;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+
+var a = 10;
+a++;
+
+console.log(a);
+--a;
+
+console.log(a);
+
+alert('Welcome to JavaScript Intro');
