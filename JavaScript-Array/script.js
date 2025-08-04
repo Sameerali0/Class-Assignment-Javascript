97f8e84ee5e6175b29d1138ad5c80e9e4f788354
Sameerali0 <sameer.ali.professional@gamail.com>
2025-08-04 12:50:09 +0500

    Javascript array

---
+++ JavaScript-Array/script.js
@@ -0,0 +1,33 @@
+//Task 1: Modify an Array Element
+let fruitNames = ["mango", "grapes", "banana"];
+console.log(fruitNames);
+
+fruitNames[1] = "apple";
+console.log(fruitNames);
+
+//Task 2: Try Changing a Character in a String
+let myName = "Sameer";
+console.log(myName);
+
+myName[0] = "Sam";
+console.log(myName);
+//It won't change. It is still the same as in the first variable.
+
+//Task 3: Replace the Whole String
+myName = "Ali";
+console.log(myName);
+
+let colours = ["blue", "green", "red"];
+console.log(colours);
+
+//pop()
+colours.pop();
+console.log(colours);
+
+//unshift()
+colours.unshift("cyan");
+console.log(colours);
+
+//shift()
+colours.shift();
+console.log(colours);
